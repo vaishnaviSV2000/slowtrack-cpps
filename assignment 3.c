8d25cf0e47d1edcbc46d03ec098276e45651ddc2
vaishnaviSV2000 <43375932+vaishnaviSV2000@users.noreply.github.com>
2018-09-27 17:16:19 +0530

    Create assignment 3.c

---
+++ assignment 3.c
@@ -0,0 +1,12 @@
+Every C program has at least one function, which is main(), and all the most trivial programs can define additional functions.
+What is trival programe?
+Trivial programes are simple pregrams.
+A function declaration tells the compiler about a function's name, return type, and parameters. A
+What is declaration?
+What is functio parameter?
+For example, strcat() to concatenate two strings, memcpy() to copy one memory location to another location, and many more functions.
+What is strac()?
+What is a string?
+What is concatenate?
+A function can also be referred as a method or a sub-routine or a procedure, etc.
+What is sub routine?
