793c0a4e5ca36e6b4cfe90f3eda0943cb12c891d
vaishnaviSV2000 <43375932+vaishnaviSV2000@users.noreply.github.com>
2018-10-01 17:31:04 +0530

    Update assignment 3.c

--- assignment 3.c
+++ assignment 3.c
@@ -1,12 +1,20 @@
 Every C program has at least one function, which is main(), and all the most trivial programs can define additional functions.
-What is trival programe?
-Trivial programes are simple pregrams.
+What is trival programs?
+Trivial programes are simple programs.
 A function declaration tells the compiler about a function's name, return type, and parameters. A
-What is declaration?
+What is function declaration?
+A declaration is a language construct that specifies properties of an identifier: it declares what a word (identifier) "means".  
 What is functio parameter?
+The term parameter is used to reffer to the variable found in the function.
 For example, strcat() to concatenate two strings, memcpy() to copy one memory location to another location, and many more functions.
-What is strac()?
+What is strcat()?
+It is a function which allows one memory block to be added at the end of second memory block.
 What is a string?
+  It is a sequence of charecters either as constants or variable.Later it can be changed or mutated.
 What is concatenate?
+  It is the operation of joining two strings.
 A function can also be referred as a method or a sub-routine or a procedure, etc.
 What is sub routine?
+  It is the sequence of program instruction which performs a specific task.
+The function name and the parameter list together constitute the function signature.
+what is signature function?
