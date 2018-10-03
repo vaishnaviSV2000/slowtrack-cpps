9e40a084f1e12dd41377a1a4202b07f990b8749b
vaishnaviSV2000 <43375932+vaishnaviSV2000@users.noreply.github.com>
2018-10-03 16:25:42 +0530

    Update assignment 3.c

--- assignment 3.c
+++ assignment 3.c
@@ -10,29 +10,27 @@ For example, strcat() to concatenate two strings, memcpy() to copy one memory lo
 What is strcat()?
 It is a function which allows one memory block to be added at the end of second memory block.
 What is a string?
-  It is a sequence of charecters either as constants or variable.Later it can be changed or mutated.
+It is a sequence of charecters either as constants or variable.Later it can be changed or mutated.
 What is concatenate?
-  It is the operation of joining two strings.
+It is the operation of joining two strings.
 A function can also be referred as a method or a sub-routine or a procedure, etc.
 What is sub routine?
-  It is the sequence of program instruction which performs a specific task.
+It is the sequence of program instruction which performs a specific task.
 The function name and the parameter list together constitute the function signature.
 what is signature function?
-  A parameter is like a placeholder.
-  What is a placeholder?
-  It defines the input and output of the function.
-  Given below is the source code for a function called max().
-    What is source code?
-    Source code is the set of instructions and statements written by a programmer using a computer programming language.
-    If a function is to use arguments, it must declare variables that accept the values of the arguments
-    What is an argument?
-    Arguments are the values supplied to the procedure when it is called.
-    What is procedure in c?
-    Small sections of code used to perform specific task.
-    These variables are called the formal parameters of the function.
-    What is formal parameter?
-    It is used to define the variable used in function.
-    This method copies the actual value of an argument into the formal parameter of the function.
+A parameter is like a placeholder.
+What is a placeholder?
+It defines the input and output of the function.
+Given below is the source code for a function called max().
+What is source code?
+Source code is the set of instructions and statements written by a programmer using a computer programming language.
+If a function is to use arguments, it must declare variables that accept the values of the arguments
+What is an argument?
+Arguments are the values supplied to the procedure when it is called.
+What is procedure in c?
+Small sections of code used to perform specific task.
+These variables are called the formal parameters of the function.
+What is formal parameter?
+It is used to define the variable used in function.
+This method copies the actual value of an argument into the formal parameter of the function.
    
-    
-    
