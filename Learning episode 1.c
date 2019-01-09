e3e6ea32dde6e28e06f3a55eb824c236ddd5d49c
vaishnaviSV2000 <43375932+vaishnaviSV2000@users.noreply.github.com>
2019-01-09 15:22:47 +0530

    Update Learning episode 1.c

--- Learning episode 1.c
+++ Learning episode 1.c
@@ -1,2 +1,9 @@
 What is github?
 Github can host and review code, manage projects, and build software.
+What is void?
+ It is used to indicate that values cannot be returned.(Source-dietel c ,pg no-42)
+  
+  
+  
+  
+  
