3f62e728a3e969e2d1e0a7cc313be1d80390748d
vaishnaviSV2000 <43375932+vaishnaviSV2000@users.noreply.github.com>
2019-01-10 16:39:30 +0530

    Create compairing 2 Nos.c

---
+++ compairing 2 Nos.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int input()
+{
+int a;
+int b;
+printf("enter the value of a");
+scanf("%d",&a);
+printf("enter the value of b");
+scanf("%d",&b);
+}
+if(a<b)
+{
+printf("%d",&a);
+}
+else(b<a)
+{
+printf("%d",&b);
+}
+int main()
+{
+
+}
