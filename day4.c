68a2dcb0eaa101089f4951c90377537e018540ba
vaishnaviSV2000 <43375932+vaishnaviSV2000@users.noreply.github.com>
2019-01-14 13:03:14 +0530

    Update day4.c

--- day4.c
+++ day4.c
@@ -49,3 +49,47 @@ int main(int x,int y,int z,int o)
         output(o,x,y,z);
 }
 
+
+
+
+    #include<stdio.h>
+int input(int *a,int *b,int *c)
+{
+        printf("enter 3 numbers");
+        scanf("%d%d%d",a,b,c);
+}
+
+int compute(int a,int b,int c)
+{
+   if(a<b && a<c)
+        {
+                printf("a is the smallest number");
+                return a;
+        }
+   else if(b<c && b<a)
+        {
+                printf("b is the smallest number");
+                return b;
+        }
+
+   else
+       {
+                printf("c is the smallest number");
+                return c;
+        }
+}
+int output(int o,int x,int y,int z)
+{
+        printf("%d is smaller than %d,%d and %d",o,x,y,z);
+        //return o;
+}
+int main ()
+{
+        int o;
+        int x;
+        int y;
+        int z;
+        input(&x,&y,&z);
+        o=compute(x,y,z);
+        output(o,x,y,z);
+}
