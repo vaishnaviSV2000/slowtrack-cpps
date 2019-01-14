3db1eaa15687daab0f8f6ec887ac7feec94b0373
vaishnaviSV2000 <43375932+vaishnaviSV2000@users.noreply.github.com>
2019-01-14 09:12:31 +0530

    Create program.c

---
+++ program.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int input(void)
+{
+    int a;
+    printf("Enter a number\n");
+    scanf("%d",&a);
+    return a;
+}
+int compute(int x)
+{
+    x=x+1;
+    return x;
+
+}
+void output(int m, int n)
+{
+    printf("%d +1 is %d",m,n);
+}
+int main(void)
+{
+    int a=input();
+    int b=compute(a);
+    output(a,b);
+}
