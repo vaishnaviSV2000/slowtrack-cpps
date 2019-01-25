1340cde231eb15fbdb13f7dc66d78e91c8a23ea3
vaishnaviSV2000 <43375932+vaishnaviSV2000@users.noreply.github.com>
2019-01-25 18:36:17 +0530

    Create day3.c

---
+++ day3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int input_1(int *a)
+{
+	int n,sum;
+	printf("enter the value of n");
+ 	scanf("%d",&n);
+	int sum[n];
+	return n;
+}
+int input_2(int n,int sum)
+{
+	int i;	
+	for(i=0;i<n;i++)
+	{
+		printf("enter the values of arry elements"),sum[n];
+	}
+}
