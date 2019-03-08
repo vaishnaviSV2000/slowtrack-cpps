13d5bda17f35256dff8297724ef722c9e2c0f407
vaishnaviSV2000 <43375932+vaishnaviSV2000@users.noreply.github.com>
2019-03-08 18:16:36 +0530

    Update addion n fractions.c

--- addion n fractions.c
+++ addion n fractions.c
@@ -1,16 +1,29 @@
 #include<stdio.h>
 struct fraction
 {
-	int num;
-	int den;
+	int n;
+	int d;
 };
-void input(int *n,struct fraction *a[n])
+struct fraction input1(int *r)
+{	
+	printf("enter the value of n");
+	scanf("%d",r);
+}	
+struct fraction input2(int r,struct fraction a[r])
 {
-	printf("enter the number of fractions to be added");
-	sacanf("%d",&n);
+	int i;
 	printf("enter the numerator");
 	printf("enter the denominator");
+	for(i=0;i<r;i++)
+	{
+		scanf("%s",&a[i].n);
+		scanf("%s",&a[i].d);
+	} 
 }
-
-	
-	
+void main()
+{
+	int i,r;
+	struct fraction a[i]; 
+	input1(&r);
+	input2(r,a[i]);
+}
