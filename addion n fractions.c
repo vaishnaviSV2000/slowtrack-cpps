fdd0bdc4f7f4a592485f95c3bad4b204fc8a5b08
vaishnaviSV2000 <43375932+vaishnaviSV2000@users.noreply.github.com>
2019-03-14 18:36:56 +0530

    Update addion n fractions.c

--- addion n fractions.c
+++ addion n fractions.c
@@ -1,3 +1,4 @@
+
 #include<stdio.h>
 struct fraction
 {
@@ -11,32 +12,35 @@ void input1(int *r)
 }	
 struct fraction input2(int *r,struct fraction a[])
 {
-	
 	int i;
-	printf("enter the numerator");
-	printf("enter the denominator");
 	for(i=0;i<*r;i++)
 	{
+		printf("enter the numerator");
 		scanf("%d",&a[i].n);
+		printf("enter the denominator");
 		scanf("%d",&a[i].d);
 	} 
 	
 }
-void compute(struct fraction a[],int *r)
+void compute(struct fraction a[],struct fraction sum,int *r)
 {
+	sum.n=1;
+	sum.d=1;
 	int i;
-	for(i=0;i<r;i++)
+	for(i=0;i<*r;i++)
 	{
-	  	a[i].n=(a[i].n*a[i].d)+(a[i].n*a[i].d);
-		a[i].d=(a[i].d*a[i].d);
-
+	  	sum.n=sum.n*a[i].d+a[i].n*sum.d;
+		sum.d=sum.d*a[i].d;
 	}
+	printf("%d/%d",sum.n,sum.d);
 }
 int main()
 {
 	int i,r;
-	struct fraction a[i]; 
+	struct fraction a[i];
+	struct fraction sum; 
 	input1(&r);
 	input2(&r,&a[r]);
-	compute(&a[i],&r);
+	compute(&a[r],sum,&r);
 }
+
