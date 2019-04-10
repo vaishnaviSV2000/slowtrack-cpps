4aad38772584334c8984de692b59f829adfb5d18
vaishnaviSV2000 <43375932+vaishnaviSV2000@users.noreply.github.com>
2019-04-10 18:21:39 +0530

    Create polynomial.c

---
+++ polynomial.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int input(int d)
+{
+	printf("enter the degree of a polynomial:\n");
+	scanf("%d",&d);
+	return d;
+}
+int input2(int d)
+{
+	int i;
+	printf("enter the cofficients;\n");
+}
